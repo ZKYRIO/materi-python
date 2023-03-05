aa5924ef0d59e60ca7529e99ccbb400fb60dd386
ZKYRIO <hamdanioke1512@gmail.com>
2023-03-05 21:10:18 +0700

    menambahkan def persegiPanjang,lingkaran,jajarGenjang dan mengedit beberapa tata letak

--- FUNCTION/soal1.py
+++ FUNCTION/soal1.py
@@ -1,12 +1,19 @@
 def pilihanUser():
     pilihan_user = int(input("Masukkan pilihan anda : "))
-    while pilihan_user > 2:
-        pilihan_user = int(
-            input("Opsi tidak ditemukan, Masukkan pilihan anda kembali : "))
     if (pilihan_user == 1):
         segitiga()
-    if (pilihan_user == 2):
+    elif (pilihan_user == 2):
         persegi()
+    elif (pilihan_user == 3):
+        persegiPanjang()
+    elif (pilihan_user == 4):
+        lingkaran()
+    elif (pilihan_user == 5):
+        jajarGenjang()
+    else:
+        print("\nOpsi tidak ditemukan, mohon pilih kembali !!")
+        pilihanUser()
+
 
 def segitiga():
     print("================================")
@@ -18,13 +25,45 @@ def segitiga():
     print("Luas segitia adalah", L)
 
 def persegi():
+    print("================================")
+    print("Program menghitung luas persegi")
+    print("================================")
     sisi = int(input("Masukkan besar sisi : "))
     L = sisi * 4
     print("Luas persegi adalah ", L)
 
+def persegiPanjang():
+    print("================================")
+    print("Program menghitung luas persegi panjang")
+    print("================================")
+    panjang = int(input("Masukkan besar panjang: "))
+    lebar = int(input("Masukkan besar lebar: "))
+    L = panjang * lebar
+    print("Luas persegi panjang adalah",L)
+
+def lingkaran():
+    print("================================")
+    print("Program menghitung luas lingkaran")
+    print("================================")
+    phi = 3.14
+    r = int(input("Masukkan besar jari jari: "))
+    L = phi * r * r
+    print("Luas lingkaran adalah", L)
+
+def jajarGenjang():
+    print("================================")
+    print("Program menghitung luas jajar genjang")
+    print("================================")
+    alas = int(input("Masukkan besar alas: "))
+    tinggi = int(input("Masukkan besar tinggi: "))
+    L = alas * tinggi
+    print("Luas jajar genjang adalah", L)
 
 print("Macam - Macam Program Perhitungan")
 print("1. Segitiga")
 print("2. Persegi")
+print("3. Persegi Panjang")
+print("4. Lingkaran")
+print("5. Jajar Genjang")
 print("\n")
 pilihanUser()
